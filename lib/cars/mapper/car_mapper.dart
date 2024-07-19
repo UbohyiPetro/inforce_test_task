adfddbf856a6cd10ba4083b328153157f13ae8ea
UbohyiPetro <upetro.2014@gmail.com>
2024-07-19 13:57:13 +0300

    ITT-7: add model mappers

---
+++ lib/cars/mapper/car_mapper.dart
@@ -0,0 +1,17 @@
+import 'package:inforce_test_task/cars/model/car_item.dart';
+import 'package:inforce_test_task/cars/model/car_local.dart';
+import 'package:inforce_test_task/core/repository/model/car_api.dart';
+
+extension CarApiToCarItem on CarApi {
+  CarItem toCarItem() {
+    return CarItem(
+      make: make,
+    );
+  }
+}
+
+extension CarLocalToCarItem on CarLocal {
+  CarItem toCarItem() {
+    return CarItem(make: make);
+  }
+}
